9363c8ee4b1d95cf2ff3910bcd8905e3f7e5c30e
AbdelrehmanElhaj <hdr333@gmail.com>
2025-03-19 02:17:39 +0000

    adding dto

---
+++ backend-RESTFulAPI/src/test/java/dev/hdrelhaj/InventoryMS/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hdrelhaj.InventoryMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
